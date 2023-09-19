0fd8617db052fc50396afb58fe9f22fb62e500da
akutubidze <akutubidze@gradcenter.cuny.edu>
2023-09-19 16:52:02 -0400

    3rd Commit

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,22 @@
 console.log('hello world');
+
+const mainTitle = document.querySelector('#title')
+let curValue = 0;
+const btnIncrease = document.querySelector('#Increase')
+const btnReset = document.querySelector('#Reset')
+const btnDecrease = document.querySelector('#Decrease')
+
+btnIncrease.addEventListener('click', () => {
+    curValue++;
+    mainTitle.textContent = curValue
+});
+
+btnDecrease.addEventListener('click', () => {
+    curValue--;
+    mainTitle.textContent = curValue
+});
+
+btnReset.addEventListener('click', () => {
+    curValue = 0;
+    mainTitle.textContent = curValue
+});
