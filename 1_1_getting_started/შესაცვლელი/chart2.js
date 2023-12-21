bffcb7390d0345285f0faee93745fc31fa659fd3
akutubidze <akutubidze@gradcenter.cuny.edu>
2023-12-21 16:49:12 -0500

    Almost there

---
+++ 1_1_getting_started/შესაცვლელი/chart2.js
@@ -0,0 +1,120 @@
+// const width = window.innerWidth * .7;
+
+const x = d3
+  .scaleLog()
+  .domain([1, 1000]) // This is what is written on the Axis: from 0 to 100
+  .range([100, 800]) // This is where the axis is placed: from 100 px to 800px
+  .base(10);
+
+d3.csv("Sorted.csv", d3.autoType).then((data) => {
+  const height = 600;
+  //   const width = window.innerWidth * 0.7;
+  const width = 800;
+
+  const margin = { top: 20, bottom: 60, left: 120, right: 40 };
+  const arr = [];
+
+  const allClass = data.map((d) => d.Classification);
+  const uniqueClass = Array.from(new Set(allClass));
+
+  const classData = [];
+
+  uniqueClass.forEach((classification) => {
+    let count = 0;
+
+    data.forEach((row) => {
+      if (row.Classification == classification) {
+        count++;
+      }
+    });
+
+    classData.push({
+      classification: classification,
+      count: count,
+    });
+  });
+  classData.sort((a, b) => a.count - b.count).reverse();
+
+  /* SCALES */
+  /** This is where you should define your scales from data to pixel space */
+  const yScale = d3
+    .scaleBand()
+    .domain(classData.map((d) => d.classification)) // აქ რა იქნება? სიესვი როგორ ავიღო?
+    .range([height, 0]);
+
+  const xScale = d3
+    .scaleLog()
+    .domain([0.5, d3.max(classData.map((d) => d.count)) + 1])
+    // .domain([1,5, 10, 100, 1000, 10000, d3.max(classData.map((d) => d.count))])
+    .range([0, width]);
+
+  /* HTML ELEMENTS */
+  // SVG
+  const svg = d3
+    .select("#chart-2-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style("background-color", "aliceblue")
+    .style("overflow", "visible");
+  //axis scales
+
+  // svg.append("g")
+  // .attr("transform", translate(${0},${height - margin.bottom})')
+  // .call(selection => selection.call(xAis))
+  // .attr("transform", 'translate(${margin.left},${0}))
+  /** Select your container and append the visual elements to it */
+
+  // BARS
+  const color = d3.scaleOrdinal(d3.schemeAccent);
+  const bars = svg
+    .selectAll("rect.bar")
+    .data(classData)
+    .join("rect")
+    .attr("class", "bar")
+    .attr("data-value", (d) => d.count)
+    .attr("x", (d) => {
+      return xScale(0);
+    })
+    .attr("y", (d) => yScale(d.classification))
+    .attr("height", 50)
+    .attr("width", 0)
+    .style("fill", "tomato")
+    .transition()
+    .duration(1500) //time in ms
+    .attr("width", function (d) {
+      return xScale(d.count);
+    })
+    .attr("height", 30)
+    .style("fill", "orange");
+
+  const yAxisGroup = svg.append("g");
+
+  yAxisGroup.call(d3.axisLeft(yScale));
+
+  const xAxisGroup = 
+
+  d3.axisBottom(xScale)
+  .tickValues([0,1,2,4, 10, 30, 100, 300, 1000, 5000,  30000])
+  .tickFormat((d, i) => {
+    console.log(d, i);
+    return d
+  })
+
+  svg.append("g")
+  .style("transform", `translate(0, ${height}px)`)
+  .call(xAxisGroup)
+
+
+
+
+  // Bar Tooltips
+  tippy('rect.bar', {
+    content: (reference) => reference.getAttribute('data-value'),
+    color: "rgb(255, 165, 0)"
+  });
+
+
+});
+
+
--- Final/chart2.js
+++ Final/chart2.js
@@ -1,22 +1,17 @@
-// const width = window.innerWidth * .7;
+// Constants and Globals
 
-const x = d3
-  .scaleLog()
-  .domain([1, 1000]) // This is what is written on the Axis: from 0 to 100
-  .range([100, 800]) // This is where the axis is placed: from 100 px to 800px
-  .base(10);
+const height = 600;
+const width = 800;
 
-console.log(x(10));
+const margin = { top: 20, bottom: 60, left: 120, right: 40 };
+
+// const x = d3.scaleLog();
+// .domain([1, 1000]); // This is what is written on the Axis: from 0 to 100
+//   .range([100, 800]); // This is where the axis is placed: from 100 px to 800px
+//   .base(10);
 
 d3.csv("Sorted.csv", d3.autoType).then((data) => {
-  const height = 600;
-  //   const width = window.innerWidth * 0.7;
-  const width = 800;
-
-  const margin = { top: 20, bottom: 60, left: 120, right: 40 };
   const arr = [];
-
-  console.log(data);
 
   const allClass = data.map((d) => d.Classification);
   const uniqueClass = Array.from(new Set(allClass));
@@ -39,13 +34,11 @@ d3.csv("Sorted.csv", d3.autoType).then((data) => {
   });
   classData.sort((a, b) => a.count - b.count).reverse();
 
-  console.log(classData);
-
   /* SCALES */
-  /** This is where you should define your scales from data to pixel space */
+
   const yScale = d3
     .scaleBand()
-    .domain(classData.map((d) => d.classification)) // აქ რა იქნება? სიესვი როგორ ავიღო?
+    .domain(classData.map((d) => d.classification))
     .range([height, 0]);
 
   const xScale = d3
@@ -53,7 +46,6 @@ d3.csv("Sorted.csv", d3.autoType).then((data) => {
     .domain([1, d3.max(classData.map((d) => d.count))])
     .range([0, width]);
 
-  /* HTML ELEMENTS */
   // SVG
   const svg = d3
     .select("#chart-2-container")
@@ -62,13 +54,6 @@ d3.csv("Sorted.csv", d3.autoType).then((data) => {
     .attr("height", height)
     .style("background-color", "aliceblue")
     .style("overflow", "visible");
-  //axis scales
-
-  // svg.append("g")
-  // .attr("transform", translate(${0},${height - margin.bottom})')
-  // .call(selection => selection.call(xAis))
-  // .attr("transform", 'translate(${margin.left},${0}))
-  /** Select your container and append the visual elements to it */
 
   // BARS
   const color = d3.scaleOrdinal(d3.schemeAccent);
@@ -77,22 +62,32 @@ d3.csv("Sorted.csv", d3.autoType).then((data) => {
     .data(classData)
     .join("rect")
     .attr("class", "bar")
-    .attr("x", (d) => {
-      console.log(xScale(10));
 
-      return xScale(0);
-    })
+    .attr("x", (d) => {})
     .attr("y", (d) => yScale(d.classification))
     .attr("height", 50)
     .attr("width", 0)
     .style("fill", "tomato")
     .transition()
-    .duration(1500) //time in ms
+    .duration(1500)
     .attr("width", function (d) {
       return xScale(d.count);
     })
+
     .attr("height", 30)
     .style("fill", "orange");
+
+  //Text
+  svg
+    .append("text")
+    .attr("x", width / 2)
+    .attr("y", height - margin.bottom + 110)
+    .style("text-anchor", "middle")
+    .attr("font-family", "Saira bold")
+    .attr("font-weight", function (d, i) {
+      return i * 100 + 100;
+    })
+    .text("Amount of artworks");
 
   const xAxisGroup = svg.append("g");
   const yAxisGroup = svg.append("g");
